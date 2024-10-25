9c9983f440249e7847436ac3abe7b80d74cf522e
Virsnt <virgisayyid@gmail.com>
2024-10-25 09:50:46 +0700

    first

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,16 +1,22 @@
 <footer class="footer py-4" style="margin-top: 120px;" data-aos="fade-up">
-    <div>
-        <h3>Organisasi</h3>
-        <p>Dewan ambalan Sastrodikoro SMK Negeri 1 Lumajang.</p>
-        <div class="social-icons">
-            <a href="#"><i class="fa-brands fa-facebook"></i></a> <!-- Facebook icon -->
-            <a href="#"><i class="fa-brands fa-instagram"></i></a> <!-- Instagram icon -->
-            <a href="#"><i class="fa-brands fa-tiktok"></i></a> <!-- TikTok icon -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="organisasi col-md-6 d-flex flex-column align-items-center mb-4 text-left">
+                <div class="social-icons">
+                    <h3>Dewan Ambalan Sastrodikoro</h3>
+                    <p>Jl. H. O.S. Cokroaminoto No.161, Tompokersan, <br> Kec. Lumajang, Kabupaten Lumajang, Jawa Timur
+                        67316.
+                    </p>
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a> <!-- Facebook icon -->
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a> <!-- Instagram icon -->
+                    <a href="#"><i class="fa-brands fa-tiktok"></i></a> <!-- TikTok icon -->
+                </div>
+            </div>
+            <div class="blog col-md-6 text-center">
+                <h3>Blog</h3>
+                <p>+6281234567890<br>
+                    sasdikscout@gmail.com</p>
+            </div>
         </div>
-    </div>
-    <div>
-        <h3>Blog</h3>
-        <p>+6281234567890<br>
-            contact@sasdikscout.com</p>
     </div>
 </footer>
