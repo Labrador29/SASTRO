bb5685166e60c5f1870db184c9d9d32a4cdf2840
Muflihmalikal <127600522+Muflihmalikal@users.noreply.github.com>
2024-12-07 11:03:44 +0700

    error qrcode

--- resources/views/admin/events/scan.blade.php
+++ resources/views/admin/events/scan.blade.php
@@ -1,63 +1,66 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <div>
-            <div id="reader" style="width: 600px"></div>
-            <input type="hidden" id="event_id" value="{{ $event->id }}">
-        </div>
+<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div>
+        <div id="reader" style="width: 600px"></div>
+        <input type="hidden" id="event_id" value="{{ $event->id }}">
     </div>
+</div>
 
-    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        function onScanSuccess(decodedText, decodedResult) {
-            $('#result').val(decodedText);
-            let id = decodedText;
-            let event_id = $('#event_id').val();
+<script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+    integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    function onScanSuccess(decodedText, decodedResult) {
+        $('#result').val(decodedText);
+        let id = decodedText;
+        let event_id = $('#event_id').val();
+        console.log(decodedText);
+        console.log(decodedResult);
 
-            html5QrcodeScanner.clear().then(_ => {
-                var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-                $.ajax({
-                    url: "{{ route('events.attendance') }}",
-                    type: 'POST',
-                    data: {
-                        _method: "POST",
-                        _token: CSRF_TOKEN,
-                        qr_code: id,
-                        event_id: event_id
-                    },
-                    success: function(response) {
-                        if (response.success) {
-                            alert(response.message);
-                        } else {
-                            alert(response.message);
-                        }
-                    },
-                    error: function() {
-                        alert('Terjadi kesalahan dalam proses absensi.');
+
+        html5QrcodeScanner.clear().then(_ => {
+            var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+            $.ajax({
+                url: "{{ route('events.attendance') }}",
+                type: 'POST',
+                data: {
+                    _method: "POST",
+                    _token: CSRF_TOKEN,
+                    qr_code: id,
+                    event_id: event_id
+                },
+                success: function(response) {
+                    if (response.success) {
+                        alert(response.message);
+                    } else {
+                        alert(response.message);
                     }
-                });
-            }).catch(error => {
-                alert('Terjadi kesalahan dalam pemindaian.');
-            });
-        }
-
-        function onScanFailure(error) {
-            console.warn(`Code scan error = ${error}`);
-        }
-
-        let html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader", {
-                fps: 10,
-                qrbox: {
-                    width: 250,
-                    height: 250
+                },
+                error: function() {
+                    alert('Terjadi kesalahan dalam proses absensi.');
                 }
-            },
-            false);
-        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-    </script>
+            });
+        }).catch(error => {
+            alert('Terjadi kesalahan dalam pemindaian.');
+        });
+    }
+
+    function onScanFailure(error) {
+        console.warn(`Code scan error = ${error}`);
+    }
+
+    let html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader", {
+            fps: 10,
+            qrbox: {
+                width: 250,
+                height: 250
+            }
+        },
+        false);
+    html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+</script>
 @endsection
